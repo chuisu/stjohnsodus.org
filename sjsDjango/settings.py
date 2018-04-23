9bb2db4e9548b8f1cc6d3beb5581f97517ae291f
chuisu <choi.brandon.h@gmail.com>
2018-04-23 16:49:27 -0400

    Playing with staticfiles, turns out the commit with changes to index.html was borked; this work needs to be redone

--- sjsDjango/settings.py
+++ sjsDjango/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'staticroot')
 MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
