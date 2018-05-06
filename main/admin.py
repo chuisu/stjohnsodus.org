e85ef38cb53e765ab2d1456377b8cf040191f02f
Brandon Choi <choi.brandon.h@gmail.com>
2018-05-05 20:44:01 -0400

    cleanup work 5/5/2018, need to finish up calendar

--- main/admin.py
+++ main/admin.py
@@ -9,27 +9,32 @@ from .models import Blog
 from .models import SplashImage
 from .models import BackgroundImage
 from .models import EmailListSignup
+import datetime
+import calendar
+from django.core.urlresolvers import reverse
+from calendar import HTMLCalendar
+from django.utils.safestring import mark_safe
 
 class AboutAdmin(admin.ModelAdmin):
     list_display = ['title' ]
-class EventAdmin(admin.ModelAdmin):
-    list_display = ['date', 'title', 'description']
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ['date', 'title', 'body']
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['date', 'title', 'description']
+    list_display = ['date', 'title', 'body']
 class SplashImageAdmin(admin.ModelAdmin):
     list_display = ['title' ]
 class BackgroundImageAdmin(admin.ModelAdmin):
     list_display = ['title' ]
 class EmailListSignupAdmin(admin.ModelAdmin):
     list_display = ['email']
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['date', 'title', 'description']
 
-admin.site.register(About)
-admin.site.register(Event)
-admin.site.register(Announcement)
-admin.site.register(Blog)
-admin.site.register(SplashImage)
-admin.site.register(BackgroundImage)
-admin.site.register(EmailListSignup)
+admin.site.register(Event, EventAdmin)
+admin.site.register(About, AboutAdmin)
+admin.site.register(Announcement, AnnouncementAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(SplashImage, SplashImageAdmin)
+admin.site.register(BackgroundImage, BackgroundImageAdmin)
+admin.site.register(EmailListSignup, EmailListSignupAdmin)
 
